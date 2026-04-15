e851576efe2ed2a11a176c5eb22398a13cf08aef
hackfisherman316-glitch <hackfisherman316@gmail.com>
2026-04-15 18:31:25 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,3 @@
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js"></script>
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
